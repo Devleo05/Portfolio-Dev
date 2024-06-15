fec3f5b8e7b270002d42d72749b78fb8043e3c22
Devleo05 <145754546+Devleo05@users.noreply.github.com>
2024-06-15 20:24:55 +0530

    bugs fixedd

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -28,12 +28,12 @@ const footer = () => {
           Copyright © 2024 Dev{" "}
         </p>
         <div className="flex items-center md:gap-3 gap-6">
-          {socialMedia.map((profile) => (
+          {socialMedia.map((info) => (
             <div
-              key={profile.id}
-              className="w-10 h-10 cursor-pointer flex justify-center items-center backdrop-filter backdrop-blur-lg saturate-18 bg-opacity-75 bg-black-200 rounded-lg border border-black-300"
+              key={info.id}
+              className="w-10 h-10 cursor-pointer flex justify-center items-center backdrop-filter backdrop-blur-lg saturate-180 bg-opacity-75 bg-black-200 rounded-lg border border-black-300"
             >
-              <img src={profile.img} alt={profile.id} width={20} height={20} />
+              <img src={info.img} alt="icons" width={20} height={20} />
             </div>
           ))}
         </div>
