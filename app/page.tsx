cebde0a483d25271a67d8c8c2a1a972d47acd38a
Devleo05 <145754546+Devleo05@users.noreply.github.com>
2024-06-15 19:44:01 +0530

    final commit

--- app/page.tsx
+++ app/page.tsx
@@ -7,12 +7,14 @@ import { FloatingNav } from "@/components/ui/FloatingNav";
 import { navItems } from "@/data";
 import Image from "next/image";
 import { FaHome } from "react-icons/fa";
+import Approach from "@/components/Approach";
+import Footer from "@/components/Footer";
 
 export default function Home() {
   return (
     <main
       className="relative bg-black-100 flex 
-    justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5"
+    justify-center items-center flex-col mx-auto sm:px-10 px-5 overflow-clip"
     >
       <div className="max-w-7xl w-full">
         <FloatingNav navItems={navItems} />
@@ -21,6 +23,8 @@ export default function Home() {
         <RecentProjects />
         <Clients />
         <Experience />
+        <Approach />
+        <Footer />
       </div>
     </main>
   );
